624c98d7476aff07d244e5a34e60e2e6dc2e4789
Marc Schleeweiß <marc.schleeweiss@gambit.de>
2023-12-07 09:11:14 +0100

    day7 improved

--- aoc2023/src/day07/README.md
+++ aoc2023/src/day07/README.md
@@ -18,3 +18,55 @@ KKKJA 2
 QQQJJ 3
 TT9TT 4
 AAAAA 5
+
+249515436
+
+```js
+const getStrength = (hand) => {
+    if (hand.includes(5)) {
+        return 6;
+    } else if (hand.includes(4)) {
+        return 5;
+    } else if (hand.includes(3) && hand.includes(2)) {
+        return 4;
+    } else if (hand.includes(3)) {
+        return 3;
+    } else if (hand.filter((c) => c === 2).length === 2) {
+        return 2;
+    } else if (hand.includes(2)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+const adjustStrength = (counts) => {
+    const strengthWithoutJokers = getStrength(counts.slice(0, counts.length - 1));
+    const jokerCount = counts[counts.length - 1];
+    let newStrength = strengthWithoutJokers;
+    if (jokerCount === 0) {
+        return strengthWithoutJokers;
+    } else if (jokerCount === 1) {
+        if (strengthWithoutJokers === 1 || strengthWithoutJokers === 2 || strengthWithoutJokers === 3) {
+            newStrength = strengthWithoutJokers + 2;
+        } else {
+            newStrength = strengthWithoutJokers + 1;
+        }
+    } else if (jokerCount === 2) {
+        if (strengthWithoutJokers === 1) {
+            newStrength = 5;
+        } else {
+            newStrength = strengthWithoutJokers + 3;
+        }
+    } else if (jokerCount === 3) {
+        if (strengthWithoutJokers === 0) {
+            newStrength = 5;
+        } else {
+            newStrength = 6;
+        }
+    } else {
+        newStrength = 6;
+    }
+    return Math.min(newStrength, 6);
+};
+```
--- aoc2023/src/day07/index.js
+++ aoc2023/src/day07/index.js
@@ -2,58 +2,9 @@ import run from "aocrunner";
 
 const parseInput = (rawInput) => rawInput.split("\n");
 
-const order = "AKQJT98765432";
-const order2 = "AKQT98765432J";
-
-const getStrength = (hand) => {
-    if (hand.includes(5)) {
-        return 6;
-    } else if (hand.includes(4)) {
-        return 5;
-    } else if (hand.includes(3) && hand.includes(2)) {
-        return 4;
-    } else if (hand.includes(3)) {
-        return 3;
-    } else if (hand.filter((c) => c === 2).length === 2) {
-        return 2;
-    } else if (hand.includes(2)) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-const adjustStrength = (counts) => {
-    const strengthWithoutJokers = getStrength(counts.slice(0, counts.length - 1));
-    const jokerCount = counts[counts.length - 1];
-    let newStrength = strengthWithoutJokers;
-    if (jokerCount === 0) {
-        return strengthWithoutJokers;
-    } else if (jokerCount === 1) {
-        if (strengthWithoutJokers === 1 || strengthWithoutJokers === 2 || strengthWithoutJokers === 3) {
-            newStrength = strengthWithoutJokers + 2;
-        } else {
-            newStrength = strengthWithoutJokers + 1;
-        }
-    } else if (jokerCount === 2) {
-        if (strengthWithoutJokers === 1) {
-            newStrength = 5;
-        } else {
-            newStrength = strengthWithoutJokers + 3;
-        }
-    } else if (jokerCount === 3) {
-        if (strengthWithoutJokers === 0) {
-            newStrength = 5;
-        } else {
-            newStrength = 6;
-        }
-    } else {
-        newStrength = 6;
-    }
-    return Math.min(newStrength, 6);
-};
 
 const part1 = (rawInput) => {
+    const order = "AKQJT98765432";
     const input = parseInput(rawInput).map((line) => line.trim());
 
     const parsed = input.map((line) => {
@@ -62,13 +13,13 @@ const part1 = (rawInput) => {
         for (const card of hand) {
             counts[order.indexOf(card)]++;
         }
-        const strength = getStrength(counts);
-        return {hand, counts, bid, strength};
+        const card_counts = counts.sort((a,b) => b-a).slice(0, 4).join("");
+        return {hand, card_counts, bid};
     });
 
     const sorted = parsed.sort((a, b) => {
-        if (a.strength !== b.strength) {
-            return a.strength - b.strength;
+        if (a.card_counts !== b.card_counts) {
+            return a.card_counts - b.card_counts;
         } else {
             for (let i = 0; i < a.hand.length; i++) {
                 if (a.hand[i] !== b.hand[i]) {
@@ -85,32 +36,36 @@ const part1 = (rawInput) => {
 };
 
 const part2 = (rawInput) => {
+    const order = "AKQT98765432J";
     const input = parseInput(rawInput).map((line) => line.trim());
 
     const parsed = input.map((line) => {
         const [hand, bid] = line.split(" ");
-        const counts = Array(order2.length).fill(0);
+        const counts = Array(order.length).fill(0);
         for (const card of hand) {
-            counts[order2.indexOf(card)]++;
+            counts[order.indexOf(card)]++;
         }
-        const strength = adjustStrength(counts);
-        return {hand, counts, bid, strength};
+        const jokerCount = counts.pop();
+        const relevantCounts = counts.sort((a,b) => b-a).slice(0, 4);
+        relevantCounts[0] += jokerCount;
+
+        const card_counts = relevantCounts.join("");
+
+        return {hand, card_counts, bid};
     });
 
     const sorted = parsed.sort((a, b) => {
-        if (a.strength !== b.strength) {
-            return a.strength - b.strength;
+        if (a.card_counts !== b.card_counts) {
+            return a.card_counts - b.card_counts;
         } else {
             for (let i = 0; i < a.hand.length; i++) {
                 if (a.hand[i] !== b.hand[i]) {
-                    return order2.indexOf(b.hand[i]) - order2.indexOf(a.hand[i]);
+                    return order.indexOf(b.hand[i]) - order.indexOf(a.hand[i]);
                 }
             }
         }
         return 0;
     });
-
-    console.log(sorted.map((s) => `${s.hand} - ${s.counts} - ${s.strength}`).join("\n"));
 
     return sorted.reduce((acc, curr, i) => {
         return acc + (i + 1) * curr.bid;
