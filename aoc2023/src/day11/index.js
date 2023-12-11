3ff89078ab0befca8de09c24ba98ecd806628d18
Marc Schleeweiß <marc.schleeweiss@gambit.de>
2023-12-11 08:49:46 +0100

    day 11 part 2

--- aoc2023/src/day11/index.js
+++ aoc2023/src/day11/index.js
@@ -3,43 +3,54 @@ import run from "aocrunner";
 const parseInput = (rawInput) => rawInput.split("\n").map((line) => line.trim().split(""));
 
 const transpose = (universe) => universe[0].map((_, colIndex) => universe.map((row) => row[colIndex]));
-const expand = (universe) => universe.flatMap(row => row.every(c => c === ".") ? [row, row] : [row]);
 
-const diffBetweenCoords = (a, b) => Math.abs(a.row - b.row) + Math.abs(a.col - b.col);
+const diffBetweenCoords = (a, b, emptyRows, emptyCols, expansion) => {
+    const [minRow, maxRow] = [Math.min(a.row, b.row), Math.max(a.row, b.row)];
+    const [minCol, maxCol] = [Math.min(a.col, b.col), Math.max(a.col, b.col)];
+    const expandedRows = emptyRows.filter(row => row > minRow && row < maxRow).length;
+    const expandedCols = emptyCols.filter(col => col > minCol && col < maxCol).length;
+    return Math.abs(a.row - b.row) + Math.abs(a.col - b.col) + (expandedRows + expandedCols) * (expansion - 1);
+}
 
+const indexIfEmpty = (row, i) => row.every(c => c === ".") ? i : -1;
+const notEmpty = i => i > -1;
 const sum = (a, b) => a + b;
 
-const part1 = (rawInput) => {
-    const input = parseInput(rawInput);
+const shortestDistances = (universe, expansion) => {
+    const emptyRows = universe
+        .map(indexIfEmpty)
+        .filter(notEmpty);
 
-    const emptyRows = input.map((row, i) => row.every(c => c === ".") ? i : -1).filter(i => i > -1);
-    const emptyCols = transpose(input).map((col, i) => col.every(c => c === ".") ? i : -1).filter(i => i > -1);
-
-    console.log({emptyRows, emptyCols});
-
-    const expandedInput = expand(transpose(expand(transpose(input))));
+    const emptyCols = transpose(universe)
+        .map(indexIfEmpty)
+        .filter(notEmpty);
 
     const coords = [];
-    for (let row = 0; row < expandedInput.length; row++) {
-        for (let col = 0; col < expandedInput[row].length; col++) {
-            if (expandedInput[row][col] === "#") {
+    for (let row = 0; row < universe.length; row++) {
+        for (let col = 0; col < universe[row].length; col++) {
+            if (universe[row][col] === "#") {
                 coords.push({ row, col });
             }
         }
     }
+    return coords
+        .map((coord, i, array) =>
+            array.slice(i + 1)
+                .map(other => diffBetweenCoords(coord, other, emptyRows, emptyCols, expansion))
+                .reduce(sum, 0)
+        )
+        .reduce(sum, 0);
+}
 
-    console.log(coords);
-    return coords.reduce((acc, coord, i, array) => {
-        const otherCoords = array.slice(i + 1);
-        const diff = otherCoords.map(otherCoord => diffBetweenCoords(coord, otherCoord)).reduce(sum, 0);
-        return acc + diff;
-    }, 0);
+const part1 = (rawInput) => {
+    const universe = parseInput(rawInput);
+    return shortestDistances(universe, 2);
 };
 
 const part2 = (rawInput) => {
-    const input = parseInput(rawInput);
-
-    return;
+    const universe = parseInput(rawInput);
+    // 100 for the testcase, 1000000 for the real input
+    return shortestDistances(universe, 1000000);
 };
 
 run({
@@ -64,12 +75,21 @@ run({
     part2: {
         tests: [
             {
-                input: ``,
-                expected: 0,
+                input: `...#......
+                .......#..
+                #.........
+                ..........
+                ......#...
+                .#........
+                .........#
+                ..........
+                .......#..
+                #...#.....`,
+                expected: 8410,
             },
         ],
         solution: part2,
     },
     trimTestInputs: true,
-    onlyTests: true,
+    onlyTests: false,
 });
