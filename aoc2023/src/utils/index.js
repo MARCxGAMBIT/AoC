558e9ea585d69e8bee7622c15e9280829a0307ec
Marc Schleeweiß <marc.schleeweiss@gambit.de>
2023-12-15 09:10:06 +0100

    add utils

--- aoc2023/src/utils/index.js
+++ aoc2023/src/utils/index.js
@@ -22,3 +22,23 @@
  *     import myLib from '../utils/myLib'
  *     import { myUtil } from '../utils'
  */
+
+export function parseInput(rawInput) {
+    return rawInput.split("\n").map((line) => line.trim());
+}
+
+export function parseGroupedInput(rawInput) {
+    return rawInput.split("\n\n").map((group) => group.split("\n").map((line) => line.trim()));
+}
+
+export function transpose(matrix) {
+    return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
+}
+
+export function sum(a, b) {
+    return a + b;
+}
+
+export function prod(a, b) {
+    return a * b;
+}
