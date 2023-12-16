7260581d7cea02d4f76f0ee9af80be269ca0cae7
Marc Schleeweiß <marc.schleeweiss@gambit.de>
2023-12-16 20:15:32 +0100

    improve day 13

--- aoc2023/src/day13/index.js
+++ aoc2023/src/day13/index.js
@@ -2,73 +2,30 @@ import run from "aocrunner";
 import { transpose, parseGroupedInput } from "../utils/index.js";
 
 const diffBetweenRows = (a, b) => {
-    let diffsAt = [];
+    let diffs = 0;
     for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-            diffsAt.push(i);
-        };
+        a[i] !== b[i] && diffs++;
     }
-    return diffsAt;
+    return diffs;
 }
 
-const findReflection = (matrix) => {
-    let reflectionAt = 0;
-    for (let i = 0; i < matrix.length - 1; i++) {
-        if (diffBetweenRows(matrix[i], matrix?.[i + 1]).length === 0) {
-            for (let j = i; j >= 0; j--) {
-                if (2 * i - j + 1 < matrix.length && diffBetweenRows(matrix[j], matrix?.[2 * i - j + 1]).length !== 0) {
+const findReflection = (matrix, tolerance) => {
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i + 1] && diffBetweenRows(matrix[i], matrix[i + 1]) <= tolerance) {
+            let diffCounter = 0;
+            let reflIdx = 0
+            for (let j = i; (j >= 0) && ((reflIdx = 2 * i - j + 1) < matrix.length); j--) {
+                diffCounter += diffBetweenRows(matrix[j], matrix[reflIdx]);
+                if (diffCounter > tolerance) {
                     break;
                 }
-                if (j === 0) {
-                    reflectionAt = i + 1;
+                if ((j === 0 || reflIdx === matrix.length - 1) && diffCounter === tolerance) {
+                    return i + 1;
                 }
             }
         }
     }
-    return reflectionAt;
-}
-
-const flip = (char) => {
-    return {
-        "#": ".",
-        ".": "#",
-    }[char];
-}
-
-
-const findReflection2 = (matrix, oldReflection) => {
-    let reflectionAt = 0;
-    for (let i = 0; i < matrix.length; i++) {
-        if (matrix?.[i + 1] && diffBetweenRows(matrix[i], matrix?.[i + 1]).length <= 1) {
-            for (let j = i; j >= 0; j--) {
-                let smudges = [];
-                const orig = matrix[j];
-                const reflection = matrix?.[2 * i - j + 1];
-
-                if (reflection) {
-                    const newSmudges = diffBetweenRows(orig, reflection);
-                    if (newSmudges.length > 1) {
-                        break;
-                    }
-                    if (newSmudges.length === 1 && smudges.length === 1) {
-                        break;
-                    }
-                    smudges = newSmudges;
-                }
-
-                if (j === 0) {
-                    reflectionAt = i + 1;
-                    if (smudges.length === 1) {
-                        matrix[j][smudges[0]] = flip(matrix[j][smudges[0]]);
-                    }
-                    if (oldReflection !== reflectionAt) {
-                        return reflectionAt;
-                    }
-                }
-            }
-        }
-    }
-    return reflectionAt;
+    return 0;
 }
 
 const part1 = (rawInput) => {
@@ -79,7 +36,7 @@ const part1 = (rawInput) => {
         const matrix = group.map(line => line.split(""));
         const transposed = transpose(matrix);
 
-        sum += 100 * findReflection(matrix) + findReflection(transposed);
+        sum += 100 * findReflection(matrix, 0) + findReflection(transposed, 0);
     }
 
     return sum;
@@ -91,21 +48,9 @@ const part2 = (rawInput) => {
     let sum = 0;
     for (const group of input) {
         const matrix = group.map(line => line.split(""));
-        const origReflectionAt = findReflection(matrix);
-        const reflectionAt = findReflection2(matrix, origReflectionAt);
+        const transposed = transpose(matrix);
 
-        let score = 0;
-        if (reflectionAt !== origReflectionAt) {
-            score = 100 * reflectionAt;
-        } else {
-            const origTransposedReflection = findReflection(transpose(matrix));
-            const transposedReflectionAt = findReflection2(transpose(matrix), origTransposedReflection);
-
-            if (transposedReflectionAt !== origTransposedReflection) {
-                score = transposedReflectionAt;
-            }
-        }
-        sum += score;
+        sum += 100 * findReflection(matrix, 1) + findReflection(transposed, 1);
     }
 
     return sum;
@@ -138,20 +83,22 @@ run({
     part2: {
         tests: [
             {
-                input: `.#.##.#.###
-..####..##.
-#########..
-.##..##..##
-.##..##..##
-#########..
-..####..##.
-.#.##.#.###
-#.#..#.##.#
-..#####.###
-...##.....#
-..#..#..#..
-...##...#..`,
-                expected: 4,
+                input: `#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#`,
+                expected: 400,
             },
         ],
         solution: part2,
