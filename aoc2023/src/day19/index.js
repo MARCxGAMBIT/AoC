769ec6216ea3fe5dfbbfbce2bb632a3b7571eb09
Marc Schleeweiß <marc.schleeweiss@gambit.de>
2023-12-20 08:24:16 +0100

    day 19

--- aoc2023/src/day19/index.js
+++ aoc2023/src/day19/index.js
@@ -53,14 +53,86 @@ const part1 = (rawInput) => {
         }
     }
 
-    const result = accepted.reduce((acc, {x,m,a,s}) => acc + x + m + a + s, 0);
+    const result = accepted.reduce((acc, { x, m, a, s }) => acc + x + m + a + s, 0);
     return result;
 };
 
 const part2 = (rawInput) => {
     const input = parseGroupedInput(rawInput);
+    const [rawWorkflows] = input;
+    const workflows = rawWorkflows.reduce((acc, workflow) => {
+        const [name, rawConditions] = workflow.split("{");
+        const conditions = rawConditions.slice(0, -1).split(",");
 
-    return;
+        acc[name] = conditions;
+        return acc;
+    }, {});
+
+    const splits = {};
+    for (const c of 'xmas') {
+        splits[c] = [1, 4000];
+    }
+
+    const accepted = [];
+    const todo = [{ splits, remainingConditions: workflows.in, result: "in" }];
+
+    while (todo.length > 0) {
+        const current = todo.shift();
+
+        const currentCondition = current.remainingConditions.shift();
+        
+        if (currentCondition === "A") {
+            accepted.push(JSON.parse(JSON.stringify(current.splits)));
+            continue;
+        }
+        if (currentCondition === "R") {
+            continue;
+        }
+
+        const match = currentCondition.match(conditionRegex);
+
+        if (!match) {
+            todo.push({
+                splits: JSON.parse(JSON.stringify(current.splits)),
+                remainingConditions: workflows[currentCondition],
+                result: currentCondition,
+            });
+        } else {
+            const { cat, op, limit, result } = match.groups;
+            const splits1 = JSON.parse(JSON.stringify(current.splits));
+            const splits2 = JSON.parse(JSON.stringify(current.splits));
+
+            if (op === ">") {
+                splits1[cat][1] = +limit;
+                splits2[cat][0] = +limit + 1;
+            } else if (op === "<") {
+                splits1[cat][0] = +limit;
+                splits2[cat][1] = +limit - 1;
+            }
+            
+            todo.push(
+                {
+                    splits: splits2,
+                    remainingConditions: ["A", "R"].includes(result) ? [result] : workflows[result],
+                    result,
+                },
+                {
+                    splits: splits1,
+                    remainingConditions: current.remainingConditions,
+                    result: current.remainingConditions,
+            }
+            );
+
+        }
+    }
+    const result = accepted
+    .map(({x,m,a,s}) => (x[1]-x[0] + 1) * (m[1]-m[0]+1) * (a[1]-a[0]+1) * (s[1]-s[0]+1))
+    .reduce((acc, v) => acc + v, 0)
+
+
+
+
+    return result;
 };
 
 run({
@@ -109,8 +181,8 @@ hdj{m>838:A,pv}
 {x=2036,m=264,a=79,s=2244}
 {x=2461,m=1339,a=466,s=291}
 {x=2127,m=1623,a=2188,s=1013}`,
-                expected: 0,
-            },
+                expected: 167409079868000,
+            }
         ],
         solution: part2,
     },
