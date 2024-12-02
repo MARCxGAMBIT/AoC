e917a65711287f9917b3071b878b1527db0f4cdf
Marc Schleeweiß <marc.schleeweiss@gambit.de>
2024-12-02 09:18:30 +0100

    solve day 2

--- aoc2024/README.md
+++ aoc2024/README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 1.026ms
-Time part 2: 1.255ms
-Both parts: 2.282ms
+Time part 1: 0.994ms
+Time part 2: 0.889ms
+Both parts: 1.883ms
 ```
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.017ms
+Time part 2: 3.694ms
+Both parts: 4.71ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 2.282ms
+Total stars: 4/50
+Total time: 6.594ms
 ```
 
 <!--/RESULTS-->
---
+++ aoc2024/src/day02/index.js
@@ -0,0 +1,106 @@
+import run from "aocrunner";
+import { parseInput, parseGroupedInput, parseMatrix, parseTable, ascending, sum } from "../utils/index.js";
+
+class Solver {
+
+  isPos = (num) => num > 0 & num < 4;
+  isNeg = (num) => num < 0 & num > -4;
+  isIncreasing = (nums) => nums.every(this.isPos);
+  isDecreasing = (nums) => nums.every(this.isNeg);
+  calcDiff = (nums) => nums.map((curr, i) => curr - nums[i + 1]).slice(0, -1);
+  isSafe = (nums) => this.isIncreasing(this.calcDiff(nums)) || this.isDecreasing(this.calcDiff(nums));
+  isNotSafe = (nums) => !this.isSafe(nums);
+
+  constructor(rawInput) {
+    this.rawInput = rawInput;
+  }
+
+  parseInput() {
+    this.table = parseTable(this.rawInput);
+    return this;
+  }
+
+  determineGoodReports() {
+    this.goodReports = this.table.filter(this.isSafe);
+    return this;
+  }
+
+  determineBadReports() {
+    this.badReports = this.table.filter(this.isNotSafe);
+    return this;
+  }
+
+  determineFixableReports() {
+    this.fixableReports = this.badReports
+      .filter(nums => nums
+        .map((_, i) => nums.toSpliced(i, 1))
+        .some(this.isSafe));
+
+    return this;
+  }
+
+  calculateSafeReports() {
+    return this.goodReports.length + (this.fixableReports?.length ?? 0);
+  }
+}
+
+/**
+ * Calculate the solution of part 1
+ * 
+ * @param {string} rawInput 
+ * @returns {Number} solution to the problem
+ */
+const part1 = (rawInput) => new Solver(rawInput)
+  .parseInput()
+  .determineGoodReports()
+  .calculateSafeReports();
+
+
+/**
+ * Calculate the solution of part 2
+ * 
+ * @param {string} rawInput 
+ * @returns {Number} solution to the problem
+ */
+const part2 = (rawInput) => new Solver(rawInput)
+  .parseInput()
+  .determineGoodReports()
+  .determineBadReports()
+  .determineFixableReports()
+  .calculateSafeReports();
+
+/**
+ * AoC Runner
+ */
+run({
+  part1: {
+    tests: [
+      {
+        input: `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`,
+        expected: 2,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`,
+        expected: 4,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
