8de8441d469640c674196f4749a4f3be1ff3d923
Marc Schleeweiß <marc.schleeweiss@gambit.de>
2024-12-03 10:08:46 +0100

    solve day 3

--- aoc2024/README.md
+++ aoc2024/README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -69,23 +69,23 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.994ms
-Time part 2: 0.889ms
-Both parts: 1.883ms
+Time part 1: 0.737ms
+Time part 2: 0.969ms
+Both parts: 1.706ms
 ```
 
 ```
 Day 02
-Time part 1: 1.017ms
-Time part 2: 3.694ms
-Both parts: 4.71ms
+Time part 1: 1.007ms
+Time part 2: 4.922ms
+Both parts: 5.929ms
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.539ms
+Time part 2: 0.933ms
+Both parts: 1.472ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 6.594ms
+Total stars: 6/50
+Total time: 9.106ms
 ```
 
 <!--/RESULTS-->
---
+++ aoc2024/src/day03/index.js
@@ -0,0 +1,81 @@
+import run from "aocrunner";
+import { parseInput, sum } from "../utils/index.js";
+
+const mulRegex = /mul\((\d+,\d+)\)/gs;
+const replaceRegex = /(don't\(\)).*?(do\(\))|(don't\(\)).*?$/g;
+
+/**
+ * Calculate the solution of part 1
+ * 
+ * @param {string} rawInput 
+ * @returns {Number} solution to the problem
+ */
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  return input
+    .flatMap(line => [...line.matchAll(mulRegex)])
+    .map(match => match[1])
+    .map(mul => mul.split(","))
+    .map(([a, b]) => a * b)
+    .reduce(sum);
+};
+
+/**
+ * Calculate the solution of part 2
+ * 
+ * @param {string} rawInput 
+ * @returns {Number} solution to the problem
+ */
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  const inputLine = input.join("");
+
+  return [
+    ...inputLine
+      .replaceAll(replaceRegex, "")
+      .matchAll(mulRegex)
+  ]
+    .map(match => match[1])
+    .map(mul => mul.split(","))
+    .map(([a, b]) => a * b)
+    .reduce(sum);
+};
+
+/**
+ * AoC Runner
+ */
+run({
+  part1: {
+    tests: [
+      {
+        input: `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+`,
+        expected: 161,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+`,
+        expected: 48,
+      },
+      {
+        input: `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+        mul(2,2)abcdon't()mul(2,5)don't()
+        mul(3,3)do()
+        mul(4,4)do()
+        mul(4,3)don't()mul(4,5)
+`,
+        expected: 80,
+      }
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
