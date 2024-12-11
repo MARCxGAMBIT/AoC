95f04286d5b967bbc4b37ceb98c2baec195a04f9
Marc Schleeweiß <marc.schleeweiss@gambit.de>
2024-12-11 17:31:32 +0100

    eslint uses globals node

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import globals from "globals";
 import js from "@eslint/js";
 
+console.log(globals);
+
 export default [
   {
     ignores: ["**/template/**/*"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: globals.node },
     rules: {
       ...js.configs.recommended.rules,
       "no-unused-vars": [
