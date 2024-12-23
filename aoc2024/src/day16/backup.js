4d63e4faae4a495ccaf6a8bcb94029035c71ded0
Marc Schleeweiß <marc.schleeweiss@gambit.de>
2024-12-23 13:54:01 +0100

    improve day 16 (but still not solved)

--- aoc2024/src/day16/backup.js
+++ aoc2024/src/day16/backup.js
@@ -1,6 +1,5 @@
 import run from "aocrunner";
 import { parseMatrix, findSymbol } from "../utils/index.js";
-import { MinPriorityQueue } from "@datastructures-js/priority-queue";
 
 const dirs = [
   [0, 1],
@@ -8,6 +7,25 @@ const dirs = [
   [0, -1],
   [-1, 0],
 ];
+
+class PriorityQueue {
+  constructor() {
+    this.elements = [];
+  }
+
+  push(item, priority, direction, path) {
+    const insertAt = this.elements.findIndex((e) => e.priority > priority);
+    this.elements.splice(insertAt, 0, { item, priority, direction, path });
+  }
+
+  shift() {
+    return this.elements.shift();
+  }
+
+  isEmpty() {
+    return this.elements.length === 0;
+  }
+}
 
 /**
  * Calculate the solution of part 1
@@ -17,40 +35,47 @@ const dirs = [
  */
 const part1 = (rawInput) => {
   const input = parseMatrix(rawInput);
+
   const start = findSymbol(input, "S");
+  const end = findSymbol(input, "E");
 
-  const queue = new MinPriorityQueue((e) => e.priority);
-  queue.enqueue({ item: start, priority: 0, direction: 0 });
+  const queue = new PriorityQueue();
 
-  const costs = { [[...start, 0]]: 0 };
-  let best = Infinity;
+  let currDir = 0;
+
+  queue.push(start, 0, currDir, [start]);
+  const costs = { [[...start, currDir]]: 0 };
 
   while (!queue.isEmpty()) {
-    const current = queue.dequeue();
+    const current = queue.shift();
     const [row, col] = current.item;
 
-    if (input[row][col] === "E") {
-      best = Math.min(best, current.priority);
-    }
-
-    for (const [direction, [dy, dx]] of dirs.entries()) {
-      const next = [row + dy, col + dx, direction];
+    for (const [dir, [dy, dx]] of dirs.entries()) {
+      const next = [row + dy, col + dx, dir];
       if (input[next[0]][next[1]] === "#") {
         continue;
       }
 
-      const priority =
+      const newCost =
         costs[[...current.item, current.direction]] +
         1 +
-        (current.direction !== direction ? 1000 : 0);
-      if (!(next in costs) || priority < costs[next]) {
-        costs[next] = priority;
-        queue.enqueue({ item: [next[0], next[1]], priority, direction });
+        (current.direction !== dir ? 1000 : 0);
+      if (!(next in costs) || newCost < costs[next]) {
+        costs[next] = newCost;
+        queue.push([next[0], next[1]], newCost, dir, [
+          ...current.path,
+          [next[0], next[1]],
+        ]);
       }
     }
   }
 
-  return best;
+  return Math.min(
+    costs[[...end, 0]] ?? Infinity,
+    costs[[...end, 1]] ?? Infinity,
+    costs[[...end, 2]] ?? Infinity,
+    costs[[...end, 3]] ?? Infinity,
+  );
 };
 
 /**
@@ -64,46 +89,45 @@ const part2 = (rawInput) => {
 
   const start = findSymbol(input, "S");
 
-  const queue = new MinPriorityQueue((e) => e.priority);
-  queue.enqueue({ item: start, priority: 0, direction: 0, path: [start] });
-
-  const costs = { [[...start, 0]]: 0 };
-  const seen = {};
+  const queue = new PriorityQueue();
+  const seen = [];
   let best = Infinity;
 
+  let currDir = 0;
+
+  queue.push(start, 0, currDir, [start]);
+  const costs = { [[...start, currDir]]: 0 };
+
   while (!queue.isEmpty()) {
-    const current = queue.dequeue();
+    const current = queue.shift();
     const [row, col] = current.item;
 
     if (input[row][col] === "E") {
-      best = Math.min(current.priority, best);
-      (seen[current.priority] ??= []).push(...current.path);
-      continue;
+      best = current.priority;
+      console.log({ best });
     }
 
-    for (const [direction, [dy, dx]] of dirs.entries()) {
-      const next = [row + dy, col + dx, direction];
+    for (const [dir, [dy, dx]] of dirs.entries()) {
+      const next = [row + dy, col + dx, dir];
       if (input[next[0]][next[1]] === "#") {
         continue;
       }
 
-      const priority =
+      const newCost =
         costs[[...current.item, current.direction]] +
         1 +
-        (current.direction !== direction ? 1000 : 0);
-      if (!(next in costs) || priority <= costs[next]) {
-        costs[next] = priority;
-        queue.enqueue({
-          item: [next[0], next[1]],
-          priority,
-          direction,
-          path: [...current.path, [next[0], next[1]]],
-        });
+        (current.direction !== dir ? 1000 : 0);
+      if (!(next in costs) || newCost < costs[next]) {
+        costs[next] = newCost;
+        queue.push([next[0], next[1]], newCost, dir, [
+          ...current.path,
+          [next[0], next[1]],
+        ]);
       }
     }
   }
 
-  return new Set(seen[best].map(([r, c]) => `${r},${c}`)).size;
+  // return new Set(seen.map(([r,c]) => `${r},${c}`)).size;
 };
 
 /**
